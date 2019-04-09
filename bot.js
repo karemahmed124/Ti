f848b9fb012670fa336a85e449bd152a778c1f5f
karemahmed124 <42273386+karemahmed124@users.noreply.github.com>
2019-04-09 19:40:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,68 @@
-const developers = ["514857011113099289"]
-const adminprefix = "R";
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "*";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : EX Clan`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Victoria ,!`,"http://twitch.tv/Death Shop")
+client.user.setStatus("dnd")
+});
+
+client.on("message", message => {
+    var prefix = "*";
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'bc') {
+        if (!args[1]) {
+    message.channel.send("bc <message>");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                m.send(args);
+            });
+            const AziRo = new Discord.RichEmbed()
+            .setAuthor(message.author.username, message.author.avatarURL)
+            .setTitle(':white_check_mark:| جاري ارسال رسالتك ') 
+            .addBlankField(true)
+            .addField(':hotsprings:| عدد الاعضاء المرسل لهم ', message.guild.memberCount , true)
+            .addField(':pencil:| الرسالة ', args)
+            .setColor('RANDOM')
+            message.channel.sendEmbed(AziRo);
+        }
+        } else {
+            return;
+        }
+    });
+
+const developers = ["562313255322779657"]
+const adminprefix = "*";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
       
-  if (message.content.startsWith(adminprefix + 'P')) {
+  if (message.content.startsWith(adminprefix + 'ply')) {
     client.user.setGame(argresult);
       message.channel.send(`**Now Playig   ${argresult}**`)
   } else 
      if (message.content === (adminprefix + "leaveserver")) {
     message.guild.leave();        
   } else  
-  if (message.content.startsWith(adminprefix + 'W')) {
+  if (message.content.startsWith(adminprefix + 'wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.send(`**Now Watching   ${argresult}**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'L')) {
+  if (message.content.startsWith(adminprefix + 'ls')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**Now Listening   ${argresult}**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'S')) {
+  if (message.content.startsWith(adminprefix + 'st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/idk");
       message.channel.send(`**Now Streaming   ${argresult}**`)
   }
